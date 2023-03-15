3ec29d5dca08f51ae0ae21f958ae77e9c7be87fe
rainbowchazer <125148013+rainbowchazer@users.noreply.github.com>
2023-03-15 22:29:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0;
+  int l = 0;
+  int r = size - 1;
+  while (l <= r) {
+    int m = (l + r) / 2;
+    if (arr[m] < value) {
+      l = m + 1;
+    } else if (arr[m] > value) {
+      r = m - 1;
+    } else {
+      count++;
+      int mid = m - 1;
+      while (mid >= l && arr[mid] == value) {
+        count++;
+        mid--;
+      }
+      mid = m + 1;
+      while (mid <= r && arr[mid] == value) {
+        count++;
+        mid++;
+      }
+      break;
+    }
+  }   
+  return count;
 }
